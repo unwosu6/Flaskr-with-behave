f73e3992e233c69f3bed3fd145f6d3f08a56f597
Elise Deitrick <edeitrick@codio.com>
2021-07-20 19:34:01 +0000

    first test

--- flaskr.py
+++ flaskr.py
@@ -6,30 +6,39 @@ from db import close_db, init_db_command
 from auth import bp
 import blog
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    proxied = FlaskBehindProxy(app)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-app = Flask(__name__, instance_relative_config=True)
-proxied = FlaskBehindProxy(app)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+    app.register_blueprint(bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
-app.teardown_appcontext(close_db)
-app.cli.add_command(init_db_command)
-app.register_blueprint(bp)
-app.register_blueprint(blog.bp)
-app.add_url_rule('/', endpoint='index')
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
-
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    create_app().run(debug=True, host="0.0.0.0")
---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import os, sys
+sys.path.append(os.path.abspath(__file__).split('/tests')[0])
+import tempfile
+
+import pytest
+from flaskr import create_app
+from db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
